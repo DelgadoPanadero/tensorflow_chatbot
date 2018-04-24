0576ab03d38ad678e8198469ae5dd29b0d19e8b7
angel.delgado <angel.delgado@sngular.team>
2018-04-24 18:16:45 +0200

    add progress_bar module

---
+++ progress_bar.py
@@ -0,0 +1,34 @@
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
+
+if __name__ == '__main__':
+
+    from time import sleep
+
+    items = list(range(0, 57))
+    l = len(items)
+
+    printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+    for i, item in enumerate(items):
+        sleep(0.1)
+        printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
